9887ab6734927522704c1a1dc5df2cc0a99f881b
Kucabara21 <75726343+Kucabara21@users.noreply.github.com>
2021-12-24 08:08:51 +0700

    test cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = client.db("discussion_forum");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: "https://stuck-forum.netlify.app" }));
+app.use(cors({ origin: "*" }));
 const connect = async () => {
   try {
     await client.connect();
