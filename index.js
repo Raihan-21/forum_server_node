cfae3370e6e88a2b2bad10f9009176e1cad3720b
Kucabara21 <75726343+Kucabara21@users.noreply.github.com>
2021-12-24 08:35:15 +0700

    fix get data error

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = client.db("discussion_forum");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: "https://stuck-forum.netlify.app" }));
 const connect = async () => {
   try {
     await client.connect();
